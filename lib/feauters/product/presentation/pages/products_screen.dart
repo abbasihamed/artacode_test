4a30d2c765b3c6298611cf25fe998fd56a0b1f51
hamed.abbasi <hamed137881@gmail.com>
2024-11-28 01:17:37 +0330

    fixed products ui

--- lib/feauters/product/presentation/pages/products_screen.dart
+++ lib/feauters/product/presentation/pages/products_screen.dart
@@ -1,6 +1,7 @@
 import 'package:artacode_test/config/router/app_path.dart';
 import 'package:artacode_test/config/theme/app_color.dart';
 import 'package:artacode_test/core/app_assets/svg_asset.dart';
+import 'package:artacode_test/core/extensions/size_extensions.dart';
 import 'package:artacode_test/core/extensions/theme_extensions.dart';
 import 'package:artacode_test/feauters/product/presentation/controller/product_controller.dart';
 import 'package:artacode_test/feauters/product/presentation/widgets/logout_button.dart';
@@ -20,118 +21,142 @@ class ProductsScreen extends StatelessWidget {
       textDirection: TextDirection.rtl,
       child: Scaffold(
         backgroundColor: context.myTheme().colorScheme.onSecondary,
-        appBar: AppBar(
-          backgroundColor: context.myTheme().colorScheme.onSecondary,
-          surfaceTintColor: Colors.transparent,
-          toolbarHeight: 160,
-          centerTitle: false,
-          title: Container(
-            color: Colors.amber,
-            child: Column(
-              children: [
-                Container(
-                  color: Colors.redAccent,
-                  child: Align(
-                    alignment: AlignmentDirectional.centerEnd,
-                    child: SvgPicture.asset(
-                      SvgAsset.i.logo,
-                      height: 140,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Text(
-                  'فروشگاه',
-                  style: context.textStyle().labelMedium?.copyWith(
-                        fontSize: 14,
-                        color: AppColor.instance.dark,
+        body: SizedBox(
+          height: double.infinity,
+          width: double.infinity,
+          child: Stack(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 150),
+                child: GetBuilder<ProductController>(
+                  builder: (ct) {
+                    return GridView.builder(
+                      itemCount: ct.products.length,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 30, vertical: 24),
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        mainAxisSpacing: 45,
+                        crossAxisSpacing: 18,
+                        mainAxisExtent: 250,
                       ),
-                ),
-              ],
-            ),
-          ),
-          actions: const [LogoutButton()],
-        ),
-        body: GetBuilder<ProductController>(
-          builder: (ct) {
-            return GridView.builder(
-              itemCount: ct.products.length,
-              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 24),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 45,
-                crossAxisSpacing: 18,
-                mainAxisExtent: 250,
-              ),
-              itemBuilder: (context, index) {
-                final data = ct.products[index];
-                return GestureDetector(
-                  onTap: () {
-                    context.goNamed(AppPathName.i.productDetail, extra: data);
-                  },
-                  child: Stack(
-                    children: [
-                      Positioned(
-                        bottom: 0,
-                        left: 0,
-                        right: 0,
-                        child: Container(
-                          height: 180,
-                          width: 153,
-                          decoration: BoxDecoration(
-                            color: AppColor.instance.white.withOpacity(.5),
-                            borderRadius: BorderRadius.circular(8),
-                            boxShadow: [
-                              BoxShadow(
-                                color: AppColor.instance.dark.withOpacity(0.1),
-                                blurRadius: 12,
-                                offset: const Offset(0, 4),
-                              )
-                            ],
-                          ),
-                          child: Column(
+                      itemBuilder: (context, index) {
+                        final data = ct.products[index];
+                        return GestureDetector(
+                          onTap: () {
+                            context.goNamed(AppPathName.i.productDetail,
+                                extra: data);
+                          },
+                          child: Stack(
                             children: [
-                              const SizedBox(height: 80),
-                              Text(
-                                data.title,
-                                style: context.textStyle().titleMedium,
+                              Positioned(
+                                bottom: 0,
+                                left: 0,
+                                right: 0,
+                                child: Container(
+                                  height: 180,
+                                  width: 153,
+                                  decoration: BoxDecoration(
+                                    color:
+                                        AppColor.instance.white.withOpacity(.5),
+                                    borderRadius: BorderRadius.circular(8),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: AppColor.instance.dark
+                                            .withOpacity(0.1),
+                                        blurRadius: 12,
+                                        offset: const Offset(0, 4),
+                                      )
+                                    ],
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      const SizedBox(height: 80),
+                                      Text(
+                                        data.title,
+                                        style: context.textStyle().titleMedium,
+                                      ),
+                                      const SizedBox(height: 12),
+                                      Text(
+                                        data.amount.seRagham(),
+                                        style: context
+                                            .textStyle()
+                                            .labelLarge
+                                            ?.copyWith(
+                                              color: AppColor.instance.gray3,
+                                              fontSize: 30,
+                                            ),
+                                      ),
+                                      Text(
+                                        'تومان',
+                                        style: context
+                                            .textStyle()
+                                            .labelLarge
+                                            ?.copyWith(
+                                              color: AppColor.instance.gray3,
+                                              fontSize: 14,
+                                            ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
                               ),
-                              const SizedBox(height: 12),
-                              Text(
-                                data.amount.seRagham(),
-                                style: context.textStyle().labelLarge?.copyWith(
-                                      color: AppColor.instance.gray3,
-                                      fontSize: 30,
-                                    ),
-                              ),
-                              Text(
-                                'تومان',
-                                style: context.textStyle().labelLarge?.copyWith(
-                                      color: AppColor.instance.gray3,
-                                      fontSize: 14,
-                                    ),
+                              Positioned(
+                                top: 0,
+                                left: 0,
+                                right: 0,
+                                child: Image.asset(
+                                  data.img,
+                                  width: 105,
+                                  height: 150,
+                                  fit: BoxFit.contain,
+                                ),
                               ),
                             ],
                           ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+              Positioned(
+                left: 0,
+                right: 0,
+                top: 20,
+                child: Container(
+                  color: context.myTheme().colorScheme.onSecondary,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Align(
+                        alignment: AlignmentDirectional.center,
+                        child: SvgPicture.asset(
+                          SvgAsset.i.logo,
+                          height: 140,
+                          width: 140,
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      Positioned(
-                        top: 0,
-                        left: 0,
-                        right: 0,
-                        child: Image.asset(
-                          data.img,
-                          width: 105,
-                          height: 150,
-                          fit: BoxFit.contain,
-                        ),
+                      Text(
+                        'فروشگاه',
+                        style: context.textStyle().labelMedium?.copyWith(
+                              fontSize: 14,
+                              color: AppColor.instance.dark,
+                            ),
                       ),
                     ],
                   ),
-                );
-              },
-            );
-          },
+                ),
+              ),
+              PositionedDirectional(
+                top: context.viewPadding().top,
+                end: 0,
+                child: const LogoutButton(),
+              ),
+            ],
+          ),
         ),
       ),
     );
