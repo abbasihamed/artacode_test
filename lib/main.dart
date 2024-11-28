e2389413aac22ecdff13fa9bb1b6f3e5a455914b
Hamed Abbasi <h.abbasi@farazware.com>
2024-11-29 02:44:09 +0330

    change for get release

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,13 @@ import 'package:artacode_test/config/router/app_router.dart';
 import 'package:artacode_test/config/theme/app_theme.dart';
 import 'package:artacode_test/di.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
   await setup();
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   runApp(const MyApp());
 }
 
@@ -16,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      title: 'گیمینو',
       theme: AppTheme.theme,
       routerConfig: AppRouter.router,
     );
